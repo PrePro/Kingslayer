11c7edb78f59ed7e033693368206baf2c0ed35c6
Reynald Brassard <reynaldbrassard@gmail.com>
2016-11-06 08:34:15 -0800

    [AI] created a placeholder unit for AI characters, Started building framework for base abstract enemy class

---
+++ Assets/Scripts/AI/EnemyBase.cs
@@ -0,0 +1,82 @@
+﻿//======================================================================================================
+// EnemyBase.cs
+// Description: 
+// Author: Reynald Brassard
+//======================================================================================================
+using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    //======================================================================================================
+    // Datatype Declaration
+    //======================================================================================================
+    public enum State
+    {
+        Idle,
+        Patrolling,
+        Attacking
+    }
+
+    enum AnimationState
+    {
+        Idle,
+        Walking,
+        Blocking,
+        Attacking
+    }
+    //======================================================================================================
+    // Member Variables
+    //======================================================================================================
+    #region MemberVariables
+    [Header("AI States")]
+    State currentState;
+    AnimationState currentAnimation;
+
+    #endregion
+
+    //======================================================================================================
+    // Properties
+    //======================================================================================================
+    #region Properties
+    public State CurrentState
+    {
+        get { return currentState; }
+    }
+
+
+    #endregion
+
+    //======================================================================================================
+    // GameObject Functions
+    //======================================================================================================
+    #region GameObject Functions
+    void Start ()
+    {
+	
+	}
+    void Update ()
+    {
+	
+	}
+    #endregion
+    //======================================================================================================
+    // Abstract Member Functions
+    //======================================================================================================
+    #region Abstract Member Functions
+   
+    public abstract void RunBehavior();
+    public abstract void UpdateAnimation();
+    public abstract void AttackEnemy();
+    public abstract void SearchForEnemy();
+    #endregion
+    //======================================================================================================
+    // Private Member Functions 
+    //======================================================================================================
+    #region Member Functions
+
+
+
+
+    #endregion
+}

34d6b1ea40d4ccf490d8aa348c71094ecda172e0
Mxeaez <brodyball@shaw.ca>
2016-11-14 16:53:49 -0800

    [Abilities] Worked on abilities along with movement

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -4,15 +4,31 @@ using System.Collections.Generic;
 
 public class PlayerDamage : MonoBehaviour
 {
+    [Header("Weapons")]
+    [Tooltip("The weapon")]
     public GameObject Sword;
     public GameObject Bullet;
     public GameObject BulletTarget;
+
     public bool isDashing = false;
-    public bool swing = false;
+
     public float dashSpeed;
+    public float dashTime;
     public float bulletSpeed;
     public float swingSpeed;
-    public List<Skills> skills; 
+    public float swingTime;
+
+    private bool swing = false;
+    public List<Skills> skills;
+
+    [SerializeField]
+    private ProjectState CurrentState;
+    private enum ProjectState
+    {
+        CanShoot,
+        IsDone
+    }
+
 
     void Start()
     {
@@ -20,6 +36,7 @@ public class PlayerDamage : MonoBehaviour
         {
             x.currentcooldown = x.cooldown;
         }
+        CurrentState = ProjectState.IsDone;
     }
     void Update()
     {
@@ -32,47 +49,56 @@ public class PlayerDamage : MonoBehaviour
             }
         }
 
-        if(isDashing)
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            if (CurrentState == ProjectState.CanShoot)
+            {
+                CurrentState = ProjectState.IsDone;
+            }
+            else
+            {
+                CurrentState = ProjectState.CanShoot;
+            }
+        }
+
+        if (isDashing)
         {
             transform.Translate((Vector3.forward * Time.deltaTime * dashSpeed));
         }
-        
-        if(swing)
+
+        if (swing)
         {
-            Sword.transform.Rotate(Vector3.forward * swingSpeed);
+            StartCoroutine("SwordSwingmove", swingTime);
         }
     }
 
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.Space)) // Dash
+        if (Input.GetKey(KeyCode.Space)) // Dash
         {
-            if(skills[0].currentcooldown >= skills[0].cooldown)
+            if (skills[0].currentcooldown >= skills[0].cooldown)
             {
-                StartCoroutine("Dashtime", 2);
+                StartCoroutine("Dashtime", dashTime);
                 skills[0].currentcooldown = 0;
             }
         }
 
-        if (Input.GetKey(KeyCode.Keypad2)) // Projectile
-        {
-            if (skills[1].currentcooldown >= skills[1].cooldown)
-            {
-                if (skills[2].currentcooldown >= skills[2].cooldown)
-                {
-                    Shoot();
-                    skills[1].currentcooldown = 0;
-                }
-            }
-        }
-
-        if (Input.GetButton("Fire1")) // Sword
+        if (Input.GetButton("Fire1") && !isDashing) // Sword
         {
             if (skills[2].currentcooldown >= skills[2].cooldown)
             {
                 //do something
                 StartCoroutine("SwordSwing", 0.5f);
                 skills[2].currentcooldown = 0;
+            }
+            if (CurrentState == ProjectState.CanShoot)
+            {
+                if (skills[1].currentcooldown >= skills[1].cooldown)
+                {
+                    Shoot();
+                    skills[1].currentcooldown = 0;
+                    CurrentState = ProjectState.IsDone;
+                }
             }
         }
     }
@@ -90,18 +116,24 @@ public class PlayerDamage : MonoBehaviour
         yield return new WaitForSeconds(waitTime);
         swing = false;
     }
- 
+    IEnumerator SwordSwingmove(float waitTime)
+    {
+        Debug.Log("Swing");
+        Sword.transform.Rotate(Vector3.back * swingSpeed);
+        yield return new WaitForSeconds(waitTime);
+        Sword.transform.Rotate(Vector3.forward * swingSpeed);
+    }
 
     void Shoot()
     {
         Vector3 firePosition = BulletTarget.transform.position;
-        GameObject b = GameObject.Instantiate(Bullet, firePosition, BulletTarget.transform.rotation) as GameObject;
+        GameObject bullet = GameObject.Instantiate(Bullet, firePosition, BulletTarget.transform.rotation) as GameObject;
 
-        if (b != null)
+        if (bullet != null)
         {
-            Rigidbody rb = b.GetComponent<Rigidbody>();
+            Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
             Vector3 force = transform.forward * bulletSpeed;
-            rb.AddForce(force);
+            rigidbody.AddForce(force);
         }
     }
 }
@@ -110,6 +142,8 @@ public class PlayerDamage : MonoBehaviour
 [System.Serializable]
 public class Skills
 {
+    [HideInInspector]
+    public string name;
     public float cooldown;
     [HideInInspector]
     public float currentcooldown;

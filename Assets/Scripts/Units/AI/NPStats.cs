e15b77d20fac88158e09a0a613aa745d72847ec3
Reynald Brassard <reynaldbrassard@gmail.com>
2016-11-26 18:14:03 -0800

    [AI] implemented fake attacking

--- Assets/Scripts/Units/AI/NPStats.cs
+++ Assets/Scripts/Units/AI/NPStats.cs
@@ -10,33 +10,20 @@ using System;
 public class NPStats : UnitStats
 {
 
-    public override void InitialStatSetup()
-    {
-    }
-
-    public override void LevelUp()
-    {
-        
-    }
-
-    public override void LoseMana(int mana)
-    {
-        
-    }
 
     public override void ReceiveDamage(int damage)
     {
-       
+        currentHealth -= damage;
     }
 
     public override void RecieveHealing(int hpHealed)
     {
-       
+        currentHealth += hpHealed;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
     }
-
-    public override void RecieveMana(int mana)
-    {
-       
-    }
+    
 
 }
--- Assets/Scripts/Units/UnitStats.cs
+++ Assets/Scripts/Units/UnitStats.cs
@@ -8,39 +8,19 @@ using System.Collections;
 
 public abstract class UnitStats : MonoBehaviour
 {
-
     [Header("Stats")]
     [SerializeField]
     [Tooltip("The max health the character can possibly have")]
-    protected int maxdHealth;
+    protected int maxHealth;
     [SerializeField]
+    [Tooltip("The character's current health")]
     protected int currentHealth;
     [SerializeField]
-    protected int maxMana;
-    [SerializeField]
-    protected int currentMana;
-    [SerializeField]
     protected int armor;
-
     
     //======================================================================================================
     // Theoretical stat system 
     //======================================================================================================
-    [SerializeField]
-    protected int level;
-    [Header("Attributes")]
-    [SerializeField]
-    protected int intelligence;
-    [SerializeField]
-    protected int strength;
-    [SerializeField]
-    protected int agility;
-
-    public abstract void InitialStatSetup();
-    public abstract void LevelUp();
     public abstract void ReceiveDamage(int damage);
     public abstract void RecieveHealing(int hpHealed);
-    public abstract void LoseMana(int mana);
-    public abstract void RecieveMana(int mana);
-
 }

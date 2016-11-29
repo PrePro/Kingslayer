cc693593b1a568ccd8e4237174534655292d4fb4
DragonSlare <caseyjaymzs@gmail.com>
2016-11-29 12:06:19 -0800

    [Dialog] Added Dialog system

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    public Text dialog;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Apples");
+            dialog.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            dialog.gameObject.SetActive(false);
+        }
+    }
+}

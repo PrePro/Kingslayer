951f9c049b4f285382959284bc51efc103d2d2e5
Casey Stewart <CaseyStewart@stu.aii.edu>
2017-01-10 19:33:58 -0800

    [Abilites] Updated 3rd ability to work with player

---
+++ Assets/Scripts/AoeAbility.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoeAbility : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Enemy")
+        {
+            
+            col.transform.position -= new Vector3 (2 , 2, 2);
+        }
+    }
+}

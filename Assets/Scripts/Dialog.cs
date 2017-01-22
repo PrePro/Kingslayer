2c01d1dd0ec6936c32625a72eea5eef7183108cc
Kalan <kalanjenkins@hotmail.com>
2017-01-21 16:10:17 -0800

    [Dialog] Switch statement added

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -5,12 +5,56 @@ using UnityEngine.UI;
 public class Dialog : MonoBehaviour
 {
     public Canvas dialog;
+    public Text[] text;
+    public int dialogueoption;
 
-    void OnTriggerEnter(Collider col)
+
+    void OnTriggerStay(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            dialog.gameObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E) == true)
+            {
+              
+                dialog.gameObject.SetActive(true);
+               
+                {
+                    switch (dialogueoption)
+                    {
+                        case 5:
+                            text[4].gameObject.SetActive(false);
+                            text[5].gameObject.SetActive(true);
+                            dialogueoption++;
+                            break;
+                        case 4:
+                            text[3].gameObject.SetActive(false);
+                            text[4].gameObject.SetActive(true);
+                            dialogueoption++;
+                            break;
+                        case 3:
+                            text[2].gameObject.SetActive(false);
+                            text[3].gameObject.SetActive(true);
+                            dialogueoption++;
+                            break;
+                        case 2:
+                            text[1].gameObject.SetActive(false);
+                            text[2].gameObject.SetActive(true);
+                            dialogueoption++;
+                            break;
+                        case 1:
+                            text[0].gameObject.SetActive(false);
+                            text[1].gameObject.SetActive(true);
+                            dialogueoption++;
+                            break;
+                        default:
+                            text[5].gameObject.SetActive(false);
+                            text[0].gameObject.SetActive(true);
+                            dialogueoption++;
+                            break;
+
+                    }
+                }
+            }
         }
     }
 
@@ -22,3 +66,6 @@ public class Dialog : MonoBehaviour
         }
     }
 }
+        
+    
+

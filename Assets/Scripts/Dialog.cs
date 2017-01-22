315109091fdbda5655d2353493b1b8e744766bf3
Kalan <kalanjenkins@hotmail.com>
2017-01-21 20:50:30 -0800

    [City] Added assets for market

--- Assets/Scripts/Dialog.cs
+++ Assets/Scripts/Dialog.cs
@@ -15,45 +15,7 @@ public class Dialog : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E) == true)
             {
-              
-                dialog.gameObject.SetActive(true);
-               
-                {
-                    switch (dialogueoption)
-                    {
-                        case 5:
-                            text[4].gameObject.SetActive(false);
-                            text[5].gameObject.SetActive(true);
-                            dialogueoption++;
-                            break;
-                        case 4:
-                            text[3].gameObject.SetActive(false);
-                            text[4].gameObject.SetActive(true);
-                            dialogueoption++;
-                            break;
-                        case 3:
-                            text[2].gameObject.SetActive(false);
-                            text[3].gameObject.SetActive(true);
-                            dialogueoption++;
-                            break;
-                        case 2:
-                            text[1].gameObject.SetActive(false);
-                            text[2].gameObject.SetActive(true);
-                            dialogueoption++;
-                            break;
-                        case 1:
-                            text[0].gameObject.SetActive(false);
-                            text[1].gameObject.SetActive(true);
-                            dialogueoption++;
-                            break;
-                        default:
-                            text[5].gameObject.SetActive(false);
-                            text[0].gameObject.SetActive(true);
-                            dialogueoption++;
-                            break;
-
-                    }
-                }
+                dialog. gameObject.SetActive(true); 
             }
         }
     }

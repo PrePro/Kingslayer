08004a4fd8ee0e03dd92870c2b8af5127937eb11
Casey Stewart <dragonslare103@gmail.com>
2017-02-15 15:59:58 -0800

    [DialogManager] Updated to make the first dialog work

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -8,20 +8,44 @@ using UnityEngine.Assertions;
 public class DialogManager : MonoBehaviour
 {
     public Canvas dialog;
-
     public List<NpcText> npcText;
-    public List<PlayerText> playerText;
+    private List<GameObject> mList;
+    //public List<PlayerText> playerText;
+    private PlayerStats playerStats;
 
+    private bool Running;
+    private Text[] Children;
+
+    private int holder;
+
+    void OnTriggerEnter(Collider col)
+    {
+        playerStats = col.GetComponent<PlayerStats>();
+        Running = true;
+        GetFirstCanvas();
+    }
+
+    void Update()
+    {
+        if (Running)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (!dialog.gameObject.activeSelf)
+                {
+                    Debug.Log("IsActive");
+                    dialog.gameObject.SetActive(true);
+                }
+            }
+            TextUpdater(Children);
+        }
+    }
 
     void OnTriggerStay(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.E))
-                if (dialog.isActiveAndEnabled == false)
-                {
-                    dialog.gameObject.SetActive(true);
-                }
+
         }
     }
 
@@ -31,35 +55,73 @@ public class DialogManager : MonoBehaviour
         {
             dialog.gameObject.SetActive(false);
         }
+        Running = false;
+    }
+
+    void TextUpdater(Text[] text)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("TextUpdater");
+            if (holder != text.Length)
+            {
+                if (holder - 1 != -1)
+                {
+                    text[holder - 1].gameObject.SetActive(false);
+                }
+                text[holder].gameObject.SetActive(true);
+                ++holder;
+            }
+            else
+            {
+                //Open the buttons
+            }
+        }
+    }
+
+    void GetFirstCanvas()
+    {
+        for (int i = 0; i <= npcText.Capacity - 1; ++i)
+        {
+            for (int j = 0; j < npcText[i].text.Length; j++)
+            {
+                mList.Add(npcText[i].text[j].gameObject);
+                mList[i].SetActive(true);
+                Children = mList[i].GetComponentsInChildren<Text>(true);
+                foreach (Text text in Children) ;
+            }
+        }
     }
 
     void Start()
     {
-        DialogDebugger();
+        mList = new List<GameObject>();
     }
 
-    void DialogDebugger()
+
+    #region ButtonClicks
+    public void ButtonOneClick()
     {
-        Assert.IsFalse((npcText.Count != playerText.Count), "[Dialog] Length main size are not equal");
-
-        for (int i = 0; i < npcText.Count; ++i)
-        {
-            Assert.IsFalse((npcText[i].text.Length != playerText[i].text.Length), "[Dialog] Length of Element " + i + " are not the same");
-        }
+        npcText[0].text[0].gameObject.SetActive(true);
+        //Delete Button
+        //Spawn new canvas connected to this one
     }
+    #endregion
+
 }
 #region NpcText Class
 [System.Serializable]
 public class NpcText
 {
-    public Text[] text;
+    public Canvas[] text;
+    public Button[] button;
 }
 #endregion
 
-#region Answer Class
-[System.Serializable]
-public class PlayerText
-{
-    public Text[] text;
-}
-#endregion
+//#region Answer Class
+//[System.Serializable]
+//public class PlayerText
+//{
+//    public Button[] text;
+//}
+//#endregion

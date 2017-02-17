f517e82627da4873913d1603df7e31491bf807ac
YashJha <yashjhaSC@gmail.com>
2017-02-17 07:40:04 -0800

    [Dialogue] Added the NPC chat assest pack.

---
+++ Assets/TurnTheGameOn/NPC Chat/Demo Scene/Demo Assets/NPCChatDemoTriggers.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCChatDemoTriggers : MonoBehaviour {
+
+	public ChatManager chatManager;
+	public Transform platformGate;
+	public bool fanOn;
+	public float fanSpeed;
+	public GameObject fanParticles;
+	public UnityStandardAssets.Utility.AutoMoveAndRotate rotateScript;
+	public Vector3 closedPosition;
+	public Vector3 openPosition;
+	public bool gateClosed;
+	public float gateSpeed;
+
+	public void ToggleFan(){
+		if (fanOn) {
+			fanOn = false;
+			fanParticles.SetActive(false);
+			rotateScript.rotateDegreesPerSecond.value.x = 0;
+		} else {
+			fanOn = true;
+			fanParticles.SetActive(true);
+			rotateScript.rotateDegreesPerSecond.value.x = 46;
+		}
+	}
+
+	public void TogglePlatformGate(){
+		if (gateClosed) {
+			gateClosed = false;
+		} else {
+			gateClosed = true;
+		}
+	}
+
+	public void ResetLevel(){
+		chatManager.currentDialogue [1] = 0;
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	void Update(){
+		if (gateClosed) {
+			platformGate.position = Vector3.MoveTowards(platformGate.position, closedPosition, gateSpeed );
+		} else {
+			platformGate.position = Vector3.MoveTowards(platformGate.position, openPosition, gateSpeed);
+		}
+	}
+
+}
+
---
+++ Assets/TurnTheGameOn/NPC Chat/Scripts/Utility/DisableOnCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableOnCollision : MonoBehaviour {
+
+	public string playername;
+	public Behaviour userControl;
+
+	void Awake(){
+		userControl = GameObject.Find(playername).GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl> ();
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.tag == "Player"){
+			//userControl = col.GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonUserControl> ();
+			userControl.enabled = false;
+			Animator anim = col.gameObject.GetComponent<Animator> ();
+			anim.SetFloat ("Forward", 0);
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.tag == "Player"){
+			userControl.enabled = true;
+		}
+	}
+
+	public void EnablePlayer(){
+		userControl.enabled = true;
+	}
+
+}

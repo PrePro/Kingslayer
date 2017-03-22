7542bdb18de503cc2f61f80597ea5c0e1e4f9153
Kalan <Kalan@GINGERSASQUACH>
2017-03-22 13:48:19 -0800

    [privo'shouse]pushing unpushed stuff

---
+++ Assets/Scripts/Ui & Menu/MenuImageUpdater.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuImageUpdater : MonoBehaviour {
+
+
+    public GameObject House;
+    public GameObject City;
+    public GameObject Crypt;
+    public GameObject Village;
+    public GameObject Barracks;
+    public GameObject Castle;
+
+    public void LoadCity()
+    {
+        SceneManager.LoadScene("FullCity");
+    }
+    public void LoadVillage()
+    {
+        SceneManager.LoadScene("Village1.1");
+    }
+    public void LoadTestWorld()
+    {
+        SceneManager.LoadScene("BaseTestingWorld");
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadPrivoHouse()
+    {
+        SceneManager.LoadScene("PrivoHouse");
+    }
+
+    public void LoadBarracks()
+    {
+        SceneManager.LoadScene("Barracks");
+    }
+
+    public void LoadCrypt()
+    {
+        SceneManager.LoadScene("CryptGrayblock");
+    }
+
+    public void LoadQuit()
+    {
+        Debug.Log("Clicked");
+
+        Application.Quit();
+    }
+
+}

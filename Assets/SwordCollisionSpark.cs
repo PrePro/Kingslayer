22901df59132274bd015e38ef376f23df0257fd5
Damon Hawkes <theurbanjellyfish@gmail.com>
2017-04-20 22:17:29 -0700

    [Crypt] Created spark effect and script that goes with it. -Damon

---
+++ Assets/SwordCollisionSpark.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordCollisionSpark : MonoBehaviour
+{
+    public ParticleSystem CollisionSpark;
+
+    void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collision col)
+    {
+        CollisionSpark.Play();
+    }
+}

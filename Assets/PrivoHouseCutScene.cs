ac71b317001f5c2ebebc1f25b97b12be992a99d6
Yash Jha <yashjhaSC@gmail.com>
2017-04-22 02:51:45 -0700

    [PrivosHouse] First version of cutscene working.

---
+++ Assets/PrivoHouseCutScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivoHouseCutScene : MonoBehaviour
+{
+
+    public QuickCutsceneController houseCutscene;
+    public bool houseCutsceneRunning = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (houseCutscene.playingCutscene == false && houseCutsceneRunning == false)
+        {
+            houseCutscene.ActivateCutscene();
+            houseCutsceneRunning = true;
+
+        }
+    }
+}

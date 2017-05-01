9e4713614e3147366ca1b3254585da538683608a
Kalan <Kalan@GINGERSASQUACH>
2017-04-30 19:30:24 -0800

    [city] stuffs

---
+++ Assets/Scripts/ToCrypt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToCrypt : MonoBehaviour {
+    private Animator myAnimator;
+	// Use this for initialization
+	void Start () {
+        myAnimator = GetComponent<Animator>();
+        if(myAnimator == null)
+        {
+            Debug.Log("Didnt get animator (ToCrypt)");
+        }
+	}
+    void OnTriggerEnter()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Didnt get animator (ToCrypt)");
+            SceneManager.LoadScene("Crypt");
+        }
+       
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

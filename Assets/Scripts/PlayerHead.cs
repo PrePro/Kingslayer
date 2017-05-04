d2fd01af5cdae6ea8e62a369808c5c7e0e707f16
Casey Stewart <dragonslare103@gmail.com>
2017-05-03 21:47:44 -0700

    [PlayerHead] Addded PlayerHead

---
+++ Assets/Scripts/PlayerHead.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHead : MonoBehaviour {
+
+    Movement movement;
+    void Start()
+    {
+        movement = GetComponentInParent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (movement.isCrouching)
+        {
+            transform.position = new Vector3(Player.Position.x, Player.Position.y, Player.Position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(Player.Position.x, Player.Position.y + 2, Player.Position.z);
+        }
+    }
+}

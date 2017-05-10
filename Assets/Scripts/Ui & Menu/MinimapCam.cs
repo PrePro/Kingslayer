ba4cb742c66e32997c83ea79a934ec44296e1272
347540 <347540@AI911LAB245P06>
2017-05-09 17:13:12 -0700

    [City] Added better dialoge feedback.

---
+++ Assets/Scripts/Ui & Menu/MinimapCam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCam : MonoBehaviour {
+    public GameObject player;
+    public float CameraHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = player.transform.position;
+        pos.y += CameraHeight;
+        transform.position = pos;
+		
+	}
+}

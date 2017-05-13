3d35a4ac49eedd50fc2da0fe22bce7759b37251d
YashJha <347540@AI911LAB235P17>
2017-05-13 13:57:25 -0700

    [Organization] Organization/

---
+++ Assets/Scripts/CollectChalice1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectChalice1 : MonoBehaviour
+{
+    public Renderer rend;
+    bool cup1Collected = false;
+
+	void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+	
+	void Update()
+    {
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player" && CupCollection.cupCount >= 1 && cup1Collected == false)
+        {
+            PuzzleWall.cupsDelivered += 1;
+            rend.enabled = true;
+            cup1Collected = true;
+
+        }
+    }
+}

93214f6054dd865b88a2cc72f01437e79edee9cd
Casey Stewart <dragonslare103@gmail.com>
2017-05-17 19:36:32 -0700

    [City] updated all city

---
+++ Assets/Scripts/ToBaseTesting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToBaseTesting : MonoBehaviour
+{
+    static public bool TurnOn;
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        TurnOn = true;
+        SceneManager.LoadScene("BaseTestingWorld");
+    }
+
+}

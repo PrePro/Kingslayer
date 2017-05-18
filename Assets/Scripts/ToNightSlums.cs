027b18637e748979b70300ad37a35f728d8c6db0
YashJha <347540@AI911LAB235P17>
2017-05-18 15:55:37 -0700

    [crypt] more loading scripts

---
+++ Assets/Scripts/ToNightSlums.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToNightSlums : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter()
+    {
+        Debug.Log("help");
+        SceneManager.LoadScene("CityNightSlum");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

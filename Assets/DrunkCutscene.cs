d401e9f35f2d74bbe8ee4e264d303ad4311a93c1
388715 <388715@AI911LAB235P16>
2017-05-21 13:29:55 -0700

    [city] still cinematic

---
+++ Assets/DrunkCutscene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkCutscene : MonoBehaviour {
+    public QuickCutsceneController drunkCutscene;
+    public bool drunkCutsceneRunning = false;
+    public Camera cinematicCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (drunkCutscene.playingCutscene == false && drunkCutsceneRunning == false)
+        {
+            drunkCutscene.ActivateCutscene();
+            StartCoroutine(CinematicHouse());
+        }
+    }
+
+    public void Endcutscene()
+    {
+        drunkCutscene.EndCutscene();
+    }
+
+    IEnumerator CinematicHouse()
+    {
+
+        yield return new WaitForSecondsRealtime(10f);
+        //cineDelete = GameObject.FindGameObjectWithTag("CineStuff");
+        //Destroy(cineDelete);
+        drunkCutscene.EndCutscene();
+        cinematicCamera.enabled = false;
+        drunkCutsceneRunning = true;
+
+    }
+}

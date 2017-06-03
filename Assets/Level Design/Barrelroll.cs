70f25743ec43f35f55e8a4f7388bfd7ed8371d19
Kalan <Kalan@GINGERSASQUACH>
2017-06-03 15:27:55 -0800

    [cityslum] barrel trap

---
+++ Assets/Level Design/Barrelroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrelroll : MonoBehaviour {
+    public Rigidbody rb;
+    public Collider coll;
+    public GameObject fence;
+    public GameObject block;
+    void Start()
+    {
+        fence.SetActive(true);
+        block.SetActive(false);
+        coll = GetComponent<Collider>();
+        coll.isTrigger = true;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            fence.SetActive(false);
+            block.SetActive(true);
+        }
+
+    }
+}

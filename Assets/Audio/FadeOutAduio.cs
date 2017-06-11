2f3c891b419052f9499fd7326ffdc6f9470585ff
Kalan <Kalan@GINGERSASQUACH>
2017-06-10 17:41:46 -0800

    [cityslum] combat audio

---
+++ Assets/Audio/FadeOutAduio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutAduio : MonoBehaviour {
+    public AudioSource audioSource;
+    public float startVolume; 
+    void Start()
+    {
+        startVolume = audioSource.volume;
+        StartCoroutine("FadeOut",startVolume);
+    }
+    // Use this for initialization
+    public IEnumerator FadeOut( float i)
+    {
+        Debug.Log(audioSource.volume);
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= i * Time.deltaTime / 10;
+            yield return null;
+        }
+        //audioSource.Stop();
+        //audioSource.volume = startVolume;
+    }
+}
---
+++ Assets/audiodefualt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiodefualt : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioSource combatSource;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (audioSource.volume <= .5)
+        {
+            audioSource.volume += 0.005f;
+        }
+        if (combatSource.volume >= 0)
+        {
+            combatSource.volume -= 0.005f;
+        }
+
+    }
+}

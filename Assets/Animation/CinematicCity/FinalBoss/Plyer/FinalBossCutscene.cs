99c3caccf0c96c18a9d59b1e39cf482c96aa78c0
CoopaKid <test@mail.com>
2017-06-13 22:03:12 -0700

    [FinalBossCinematic] Final boss cinematic, some shots done.

---
+++ Assets/Animation/CinematicCity/FinalBoss/Plyer/FinalBossCutscene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalBossCutscene : MonoBehaviour {
+    //public GameObject player1;
+    public QuickCutsceneController qcc;
+   // public GameObject player2;
+    //public Camera cam1;
+	// Use this for initialization
+	void Start ()
+    {
+
+        qcc.ActivateCutscene();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

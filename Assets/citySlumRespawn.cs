4a6cd9d45e694c171a982dba685c97d240af2b5f
347540 <347540@AI911LAB234P11>
2017-06-15 12:42:39 -0700

    [City/CitySlum/Crypt] Getting build ready, small fixes.

---
+++ Assets/citySlumRespawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class citySlumRespawn : MonoBehaviour {
+    public GameObject startpos;
+    public GameObject respawn;
+	// Use this for initialization
+	void Start () {
+        startpos.transform.position = respawn.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

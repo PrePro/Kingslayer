e970dcac73e2f396146e4b09be2e1fb9aba5f0b3
384493 <384493@AI911LAB235P18>
2017-06-15 03:43:55 -0700

    [FinalBossCutscene] Getting there.

--- Assets/Animation/CinematicCity/FinalBoss/Plyer/FinalBossCutscene.cs
+++ Assets/Animation/CinematicCity/FinalBoss/Plyer/FinalBossCutscene.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FinalBossCutscene : MonoBehaviour {
     //public GameObject player1;
     public QuickCutsceneController qcc;
+    public QuickCutsceneController qcc2;
     public GameObject player2;
     private Animator myanim;
     //public Camera cam1;
@@ -26,12 +27,44 @@ public class FinalBossCutscene : MonoBehaviour {
     IEnumerator talkingMan()
     {
         yield return new WaitForSeconds(7f);
+        qcc2.ActivateCutscene();
         player2.SetActive(true);
+
     }
 
     IEnumerator pointingMan()
     {
         yield return new WaitForSeconds(8.5f);
         myanim.SetInteger("StopPoint", 1);
+    }
+    IEnumerator wizardTalk1()
+    {
+        Time.timeScale = .5f;
+        yield return new WaitForSeconds(5.5f);
+        Time.timeScale = 1;
+        //privoTalk();
+    }
+    IEnumerator privoTalk1()
+    {
+        yield return new WaitForSeconds(2f);
+        Time.timeScale = .5f;
+        princeTalk();
+    }
+
+    public void privoTalk()
+    {
+        if(Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+        StartCoroutine("privoTalk1");
+    }
+    public void princeTalk()
+    {
+        if (Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+        StartCoroutine("wizardTalk1");
     }
 }
